f326b2b21ebce62522e9ec627016da1f0da4fbe8
spiritxhx <spiritxhx@gmail.com>
2019-06-20 22:08:08 -0400

    import all the page files in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+
 function App() {
   return <>hello from app</>;
 }
