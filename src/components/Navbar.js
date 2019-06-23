eaf0c0bbe20eba88f40cf3fc9487fb3a8acc59db
spiritxhx <spiritxhx@gmail.com>
2019-06-23 11:32:55 -0400

    import logo and icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa';
 
 export default class Navbar extends Component {
   render() {
